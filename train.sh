b356b3652a196066a53730b13bc8413818da8664
aki202 <okashimo23@gmail.com>
2020-12-02 01:01:00 +0900

    good

--- train.sh
+++ train.sh
@@ -4,10 +4,11 @@ devices=$1
 save_name=$2
 
 CUDA_VISIBLE_DEVICES=$devices python ./src/train.py --dataset ./data/ \
+--batch_size=8 \
 --cuda \
---epoch 60 \
+--epoch 20 \
 --beam_size 2 \
 --model_name table \
 --sentence_features \
 --column_pointer \
---save $save_name
+--save saved_b_model/$save_name
